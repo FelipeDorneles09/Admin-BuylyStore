00990bb7aece3f52e89d04ddec17b8735150615d
Felipe <fdorneles63@gmail.com>
2024-12-08 20:53:51 -0300

    order details page

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -3,7 +3,9 @@ import { columns } from "@/components/orderItems/OrderItemsColumns";
 import React from "react";
 
 const OrderDetails = async ({ params }: { params: { orderId: string } }) => {
-  const res = await fetch(`http://localhost:3001/api/orders/${params.orderId}`);
+  const res = await fetch(
+    `${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`
+  );
   const { orderDetails, customer } = await res.json();
 
   const { street, city, state, postalCode, country } =
